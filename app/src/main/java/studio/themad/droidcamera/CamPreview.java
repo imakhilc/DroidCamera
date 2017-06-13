58f7aeb73119e7e148baa60ce972b268a42374e8
imAKHILc <imakhilc@gmail.com>
2017-06-13 18:54:44 +0530

    Camera API Implemented

---
+++ app/src/main/java/studio/themad/droidcamera/CamPreview.java
@@ -0,0 +1,71 @@
+package studio.themad.droidcamera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by AKHIL on 13-Jun-17.
+ */
+
+public class CamPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CamPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+}
--- app/src/main/java/studio/themad/droidcamera/CameraActivity.java
+++ app/src/main/java/studio/themad/droidcamera/CameraActivity.java
@@ -1,13 +1,65 @@
 package studio.themad.droidcamera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.FrameLayout;
 
-public class CameraActivity extends AppCompatActivity {
+import android.app.Activity;
+
+
+public class CameraActivity extends Activity {
+    private Camera mCamera;
+    private CamPreview mPreview;
+    Activity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        context = this;
+
+        //Camera permission request for Marshmallow  devices
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        } else {
+            //start camera preview if permitted
+            startPreview();
+        }
+    }
+
+    public void startPreview() {
+        mCamera = getCameraInstance();
+
+        mPreview = new CamPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.preview);
+        preview.addView(mPreview);
+    }
+
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            //attempt to get a Camera instance
+            c = Camera.open();
+        } catch (Exception e) {
+            //camera not available
+        }
+        return c;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        } else {
+            startPreview();
+        }
     }
 }
